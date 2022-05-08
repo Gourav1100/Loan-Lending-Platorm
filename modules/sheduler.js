1b5c0c2b50fd6d61df25554f3e21971c41e5a550
ShisuiMadara <2020ucs0090@iitjammu.ac.in>
2022-05-08 18:34:15 +0530

    added sheduler

--- modules/sheduler.js
+++ modules/sheduler.js
@@ -1,8 +1,13 @@
 var garbage = require("./garbageCollector");
 
-var now = new Date();
-var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0) - now;
-if (millisTill10 < 0) {
-     millisTill10 += 86400000; 
+async function shedule(req, res)
+{
+    var now = new Date();
+    var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0) - now;
+    if (millisTill10 < 0) {
+         millisTill10 += 86400000; 
+    }
+    setInterval(Beyond, millisTill10);
 }
-setInterval(Beyond, millisTill10);
+
+exports.execute = shedule;
