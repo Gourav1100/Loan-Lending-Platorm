1deb672c3a612aa4df1d89f05bd4b6fa51209535
Gourav1100 <gauravb1100@gmail.com>
2022-05-08 15:11:02 +0530

    updated header, login, and theme

--- frontend/src/pages/login/login.js
+++ frontend/src/pages/login/login.js
@@ -5,75 +5,56 @@ import {
   Row,
   Col,
   Stack,
+  Button,
   InputGroup,
   FormControl,
 } from "react-bootstrap";
+import Header from "../../components/header/header";
 import userimg from "../../icons/user.png";
 import padlock from "../../icons/padlock.png";
 import google from "../../icons/google.png";
-import "../../common.css";
-
-var old_classes = {
-
-};
+import "../../common.css"
 
 class Login extends react.Component {
-  submit = (event) => {
-    event.preventDefault();
-    console.log(event.target.username.value);
-  };
-  check = (event) => {
-    console.log(event.target.value);
-  }
   render() {
     return (
       <>
+        <Header />
         <Container>
           <Row className="vcon">
-            <Col xs={{ span: 1 }} md={{ span: 3 }} lg={{ span: 4 }} />
-            <Col xs={{ span: 10 }} md={{ span: 6 }} lg={{ span: 4 }}>
+            <Col xs={{ span: 1 }} md={{ span: 2 }}  lg={{span: 3}} xl={{span: 4}}/>
+            <Col xs={{ span: 10 }} md={{ span: 8 }} lg={{span: 6}} xl={{span: 4}}>
               <form onSubmit={this.submit}>
                 <Stack gap={2}>
                   <InputGroup className="mb-3 ">
-                    <InputGroup.Text id="basic-addon1"><img className="img_size" src={userimg} alt="username"></img></InputGroup.Text>
+                    <InputGroup.Text id="basic-addon1"><img className="img_size" src={userimg}></img></InputGroup.Text>
                     <FormControl
                       placeholder="Username"
                       aria-label="Username"
                       aria-describedby="basic-addon1"
-                      name="username"
-                      onKeyUp={this.check}
                     />
                   </InputGroup>
 
                   <InputGroup className="mb-3 ">
-                    <InputGroup.Text id="basic-addon1"><img className="img_size" src={padlock}  alt="password"></img></InputGroup.Text>
+                    <InputGroup.Text id="basic-addon1"><img className="img_size" src={padlock}></img></InputGroup.Text>
                     <FormControl
                       placeholder="Password"
                       aria-label="Password"
                       aria-describedby="basic-addon1"
-                      name="password"
-                      onKeyUp={this.check}
                     />
                   </InputGroup>
 
-                  <button type="submit" className="lg_font button_bg p-1" >
+                  <button className="button_bg p-1" onClick={this.clicked}>
                     Login
                   </button>
                   <hr className="h_break"></hr>
-                  <button type="submit" className="button_bg">
-                    Sign in with <img className="img_size" src={google} alt="Google" />
-                  </button>
+                  <button className="button_bg"><span className="lg_font">Sign in with</span> <img className="img_size" src={google}></img></button>
                   <hr className="h_break"></hr>
-                  <div>
-                    Didn't have an account ? <a href="/signup">Signup</a>
-                  </div>
                 </Stack>
               </form>
 
             </Col>
-            <Col xs={{ span: 1 }} md={{ span: 3 }} lg={{ span: 4 }} >
-            </Col>
-
+            <Col xs={{ span: 1 }} md={{ span: 2 }} lg={{span: 3}} xl={{span: 4}}/>
           </Row>
 
         </Container>
