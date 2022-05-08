7a7b8d5b38f7c5a5c41a2cd2765af72932fe46e2
ShisuiMadara <2020ucs0090@iitjammu.ac.in>
2022-05-08 15:32:37 +0530

    pushing api again

--- modules/garbageCollector.js
+++ modules/garbageCollector.js
@@ -1,8 +1,20 @@
+const URI = "mongodb+srv://manchurianhotdog:VUpYHt2jaG9IiRW4@fliprr.jbq9y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const Mongodb = require("mongodb");
+const client = new Mongodb.MongoClient(URI);
+
 function Beyond20days(req, res){
     
     try{
         await client.connect();
 
-        let db
+        const db = client.db("Flipr");
+        
+
+    }catch (err) {
+        // return the error
+        return {
+            message: new err.message,
+            success: false,
+        };
     }
 }
