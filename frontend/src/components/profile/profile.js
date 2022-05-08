d06862ec328cbb7a2c692f05beedad754cea66bb
Ashutosh-codes <ashutoshgangwar312@gmail.com>
2022-05-09 01:02:58 +0530

    completed profile component for dashboard and completed dashboard header

---
+++ frontend/src/components/profile/profile.js
@@ -0,0 +1,156 @@
+import react from "react";
+import "./profile.css";
+import {
+  Tabs,
+  Tab,
+  Modal,
+  Row,
+  Button,
+  Col,
+  Form,
+  Card,
+  Container,
+} from "react-bootstrap";
+import profile from "../../icons/profile.png";
+
+class Profile extends react.Component {
+  tab = (<>&ensp;&emsp;</>);
+  two_space = (<>&ensp;</>);
+  one_space = (<>&nbsp;</>);
+  four_space = (<>&emsp;</>);
+  render(props) {
+    return (
+      <Container>
+        <Row>
+          <Col md={{ span: 5 }} className="center mt-5 p-5">
+            <img src={profile} className="fit center"></img>
+          </Col>
+          <Col className=" p-2 mt-2">
+            <h1 className="center">Personal Details</h1>
+            <hr className="h_break"></hr>
+            <br></br>
+            <h3>
+              Name{this.tab}
+              {this.tab}
+              {this.two_space}:{this.tab}
+              {this.tab}
+              {this.props.name}
+            </h3>
+            <h3>
+              Username{this.tab}:{this.tab}
+              {this.tab}
+              {this.props.username}
+            </h3>
+            <h3>
+              Phone{this.tab}
+              {this.tab}
+              {this.one_space}:{this.tab}
+              {this.tab}
+              {this.props.phone}
+            </h3>
+            <h3>
+              Email{this.tab}
+              {this.tab}
+              {this.two_space}
+              {this.one_space}:{this.tab}
+              {this.tab}
+              {this.props.Email}
+            </h3>
+            <h3>
+              Address{this.tab}
+              {this.four_space}:{this.tab}
+              {this.tab}
+              {this.props.address}
+            </h3>
+            <h3>
+              Country{this.tab}
+              {this.four_space}:{this.tab}
+              {this.tab}
+              {this.props.country}
+            </h3>
+            <br></br>
+            <br></br>
+            <hr className="h_break"></hr>
+            <h1 className="center">Bank Details</h1>
+            <hr className="h_break"></hr>
+            <br></br>
+            <h3>
+              Aadhar Number{this.tab}:{this.tab}
+              {this.tab}
+              {this.props.aadhar}
+            </h3>
+            <h3>
+              PAN Number{this.tab}
+              {this.four_space}
+              {this.one_space}:{this.tab}
+              {this.tab}
+              {this.props.pan}
+            </h3>
+            <h3>
+              Bank{this.tab}
+              {this.tab}
+              {this.tab}
+              {this.tab}
+              {this.two_space}:{this.tab}
+              {this.tab}
+              {this.props.bank}
+            </h3>
+            <h3>
+              Branch{this.tab}
+              {this.tab}
+              {this.tab}
+              {this.four_space}:{this.tab}
+              {this.tab}
+              {this.props.branch}
+            </h3>
+            <h3>
+              IFSC Code{this.tab}
+              {this.tab}
+              {this.two_space}
+              {this.two_space}
+              {this.one_space}:{this.tab}
+              {this.tab}
+              {this.props.ifsc}
+            </h3>
+            <h3>
+              CTC{this.tab}
+              {this.tab}
+              {this.tab}
+              {this.tab}
+              {this.two_space}
+              {this.two_space}:{this.tab}
+              {this.tab}
+              {this.props.ctc}
+            </h3>
+            <br></br>
+            <br></br>
+            <hr className="h_break"></hr>
+            <h1 className="center">Loan Details</h1>
+            <hr className="h_break"></hr>
+            <h3>
+              Active Loans{this.tab}
+              {this.tab}
+              {this.two_space}
+              {this.one_space}:{this.tab}
+              {this.tab}
+              {this.props.activeloans}
+            </h3>
+            <h3>
+              Completed Loans{this.tab}:{this.tab}
+              {this.tab}
+              {this.props.completedloans}
+            </h3>
+            <h3>
+              Offered Loans{this.tab}
+              {this.tab}:{this.tab}
+              {this.tab}
+              {this.props.offeredloans}
+            </h3>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Profile;
--- frontend/src/pages/dashboard/dashboard.js
+++ frontend/src/pages/dashboard/dashboard.js
@@ -1,11 +1,52 @@
 import react from "react";
 import "./dashboard.css";
+import Dheader from "../../components/dheader/dheader.js"
+import Header from  "../../components/header/header.js"
+import Profile from "../../components/profile/profile.js"
+import {Tabs, Tab, Modal, Row, Button, Col, Form, Card, Container} from "react-bootstrap";
+
 
 class Dashboard extends react.Component {
     render() {
         return (
             <>
-                Dashboard
+               <Dheader username="Ashutosh"/>
+               <Container className="bg-dark flex-column mt-2 p-2">
+                 <Row>
+                     <Col>
+                         <Tabs defaultActiveKey="profile" 
+                               id="controlled-tab" className="tabs">
+                             <Tab eventKey="profile" title="Profile" className="tabs">
+                                 <Profile 
+                                 name={window.sessionStorage.getItem("name")}
+                                 username={window.sessionStorage.getItem("username")}
+                                 phone={window.sessionStorage.getItem("phone")}
+                                 Email={window.sessionStorage.getItem("Email")}
+                                 address={window.sessionStorage.getItem("address")}
+                                 country = {window.sessionStorage.getItem("country")}
+                                 aadhar = {window.sessionStorage.getItem("aadhar")}
+                                 pan = {window.sessionStorage.getItem("pan")}
+                                 bank = {window.sessionStorage.getItem("bank")}
+                                 branch = {window.sessionStorage.getItem("branch")}
+                                 ifsc = {window.sessionStorage.getItem("ifsc")}
+                                 ctc = {window.sessionStorage.getItem("ctc")}
+                                 activeloans = {window.sessionStorage.getItem("activeloans")}
+                                 completedloans = {window.sessionStorage.getItem("completedloans")}
+                                 offeredloans = {window.sessionStorage.getItem("offeredloans")}
+                                 
+                                 />
+                             </Tab>
+                             <Tab eventKey="home" title="Requests">
+                                 {/* <Home /> */}
+                                 <Header />
+                             </Tab>
+                             <Tab eventKey="contact" title="Contact" className="tabs">
+                                 {/* <Contact /> */}
+                             </Tab>
+                         </Tabs>
+                     </Col>
+                 </Row>
+             </Container>
             </>
         );
     }
