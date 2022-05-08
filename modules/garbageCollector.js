18e42483c49c2353e7465593332176d94e9248d1
TejaswiBishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2022-05-08 17:22:17 +0530

    Beyond20Days Garbage Collector added.

--- modules/garbageCollector.js
+++ modules/garbageCollector.js
@@ -2,19 +2,28 @@ const URI = "mongodb+srv://manchurianhotdog:VUpYHt2jaG9IiRW4@fliprr.jbq9y.mongod
 const Mongodb = require("mongodb");
 const client = new Mongodb.MongoClient(URI);
 
-function Beyond20days(req, res){
+async function Beyond20days(req, res){
     
     try{
+        var d = new Date();
+        d.setHours(0,0,0);
+        d.setDate(d.getDate() - 20);
         await client.connect();
-
         const db = client.db("Flipr");
-        
+        await db.collection("LoanRequest").deleteMany({
+            "Date" : {$lt : d}
+        });
+        return res.status(200).json({
+            success: true,
+            message: "success",
+        });
 
     }catch (err) {
         // return the error
-        return {
+        return res.status(200).json({
             message: new err.message,
             success: false,
-        };
+        });
     }
 }
+exports.execute = Beyond20days;
