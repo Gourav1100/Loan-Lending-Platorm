9c081ba2d03dbc7e68fedacba192c6cfbcb894ea
TejaswiBishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2022-05-10 04:43:06 +0530

    _id to username added.

--- api/database.js
+++ api/database.js
@@ -23,6 +23,11 @@ async function get(req,res){
         else if (query.type == "MoneyBorrowed"){
             rdata = await db.collection("LoanHistory").find({"borrower": Mongodb.ObjectID(query.userid)}).toArray();
         }
+        else if (query.type == "GetUsername"){
+            rdata = await db.collection("Users").find({"_id": Mongodb.ObjectID(query.userid)}).toArray();
+            rdata = rdata[0].username;
+            return rdata;
+        }
         else {
             rdata = await db.collection(query.type).find().toArray();
         }
