4175af8cc3c0ce935b0d27d0e796cdd3d9f089a8
Ashutosh-codes <ashutoshgangwar312@gmail.com>
2022-05-10 11:18:53 +0530

    added avatar and ui of profile

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -21,69 +21,84 @@ class Profile extends react.Component {
         <Row>
           <Col md={{ span: 5 }} className="center mt-5 p-5">
             <img src= {this.props.image? this.props.image : profile} className="fit center"></img>
+            <InputGroup className="mb-3 p-2">
+                    <InputGroup.Text id="basic-addon1">Avatar </InputGroup.Text>
+                    <FormControl
+                      type="file"
+                      name="avatar"
+                      onChange={this.loadimages}
+                      aria-label="sslips"
+                      aria-describedby="basic-addon1"
+                    />
+                    <button
+                    className="button_bg p-2"
+                    type="submit">
+                    Submit
+                  </button>
+                  </InputGroup>
           </Col>
-          <Col className=" p-2 mt-2">
+          <Col className=" p-1 mt-2">
             <h1 className="center">Personal Details</h1>
             <hr className="h_break"></hr>
             <br></br>
-            <h3>
+            <h5>
               Name{this.tab}
               {this.tab}
               {this.two_space}:{this.tab}
               {this.tab}
               {this.props.name}
-            </h3>
-            <h3>
+            </h5>
+            <h5>
               Username{this.tab}:{this.tab}
               {this.tab}
               {this.props.username}
-            </h3>
-            <h3>
+            </h5>
+            <h5>
               Phone{this.tab}
               {this.tab}
               {this.one_space}:{this.tab}
               {this.tab}
               {this.props.phone}
-            </h3>
-            <h3>
+            </h5>
+            <h5>
               Email{this.tab}
               {this.tab}
               {this.two_space}
               {this.one_space}:{this.tab}
               {this.tab}
               {this.props.Email}
-            </h3>
-            <h3>
+            </h5>
+            <h5>
               Address{this.tab}
               {this.four_space}:{this.tab}
               {this.tab}
               {this.props.address}
-            </h3>
-            <h3>
+            </h5>
+            <h5>
               Country{this.tab}
               {this.four_space}:{this.tab}
               {this.tab}
               {this.props.country}
-            </h3>
+            </h5>
             <br></br>
             <br></br>
             <hr className="h_break"></hr>
             <h1 className="center">Bank Details</h1>
             <hr className="h_break"></hr>
             <br></br>
-            <h3>
+            <h5>
               Aadhar Number{this.tab}:{this.tab}
               {this.tab}
               {this.props.aadhar}
-            </h3>
-            <h3>
+            </h5>
+            <h5>
               PAN Number{this.tab}
               {this.four_space}
               {this.one_space}:{this.tab}
               {this.tab}
               {this.props.pan}
-            </h3>
-            <h3>
+            </h5>
+            <h5>
               Bank{this.tab}
               {this.tab}
               {this.tab}
@@ -91,16 +106,16 @@ class Profile extends react.Component {
               {this.two_space}:{this.tab}
               {this.tab}
               {this.props.bank}
-            </h3>
-            <h3>
+            </h5>
+            <h5>
               Branch{this.tab}
               {this.tab}
               {this.tab}
               {this.four_space}:{this.tab}
               {this.tab}
               {this.props.branch}
-            </h3>
-            <h3>
+            </h5>
+            <h5>
               IFSC Code{this.tab}
               {this.tab}
               {this.two_space}
@@ -108,8 +123,8 @@ class Profile extends react.Component {
               {this.one_space}:{this.tab}
               {this.tab}
               {this.props.ifsc}
-            </h3>
-            <h3>
+            </h5>
+            <h5>
               CTC{this.tab}
               {this.tab}
               {this.tab}
@@ -118,25 +133,25 @@ class Profile extends react.Component {
               {this.two_space}:{this.tab}
               {this.tab}
               {this.props.ctc}
-            </h3>
+            </h5>
             <br></br>
             <br></br>
             <hr className="h_break"></hr>
             <h1 className="center">Loan Details</h1>
             <hr className="h_break"></hr>
-            <h3>
+            <h5>
               Active Loans{this.tab}
               {this.tab}
               {this.two_space}
               {this.one_space}:{this.tab}
               {this.tab}
               {this.props.activeloans}
-            </h3>
-            <h3>
+            </h5>
+            <h5>
               Completed Loans{this.tab}:{this.tab}
               {this.tab}
               {this.props.completedloans}
-            </h3>
+            </h5>
             <br />
             <hr className = "h_break"></hr>
             <h1 className="center">Update Salary Slips</h1>
